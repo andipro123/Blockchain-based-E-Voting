206ec047d1860ab3610664325846513a17246f88
AnikJ <jayateerth.aniket@gmail.com>
2020-05-01 23:47:35 +0530

    deleted extra folder

--- block/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BallotConfig(AppConfig):
-    name = 'ballot'
--- block/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'ballot'
-urlpatterns = [
-    path('create/', views.create, name='create'),
-    path('seal/', views.seal, name='seal'),
-]
